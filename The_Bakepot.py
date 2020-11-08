a2d54681edfb99e9682f2ac21eed6a0c973dded8
Karthik-Shenoy <karthikshenoy2001@gmail.com>
2020-11-08 13:50:16 +0530

    updated The_bakepot.py and linked static files

--- The_Bakepot.py
+++ The_Bakepot.py
@@ -1,2 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+	return
+
+@app.route("/shop")
+def shopPage():
+	return render_template("shop.html")
+
+@app.route("/Contact-Us")
+def contactPage():
+	return render_template("Contact_US.html")
+
+if __name__ == "__main__":
+	app.run(debug = True)
 
 
