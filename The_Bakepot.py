b26b24fa097c975816baffa229fda1fe7a42bb96
keerthi Kamath <keerthik.is19@sahyadri.edu.in>
2020-11-08 15:57:54 +0530

    Updated css

--- The_Bakepot.py
+++ The_Bakepot.py
@@ -14,6 +14,10 @@ def shopPage():
 def contactPage():
 	return render_template("Contact_US.html")
 
+@app.route("/contribute")
+def contribute():
+	return render_template("contribute.html")
+
 if __name__ == "__main__":
 	app.run(debug = True)
 
